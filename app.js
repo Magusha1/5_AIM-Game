e587b32bc7eac9f06454db28f3f24d0034473021
Mugi-Star <90766470+Mugi-Star@users.noreply.github.com>
2022-10-24 16:14:46 +0400

    5_AIM-Game

---
+++ app.js
@@ -0,0 +1,98 @@
+const startBtn = document.querySelector('#start')
+const screens = document.querySelectorAll('.screen')
+const timeList = document.querySelector('#time-list')
+const timeEL = document.querySelector('#time')
+const board = document.querySelector('#board')
+const colors = [
+  'orange',
+  '#c31c4',
+  '#e06c6c',
+  '#f246e1',
+  '#6953a3',
+  '#db6735',
+  '#2ac0d4',
+]
+
+let time = 0
+let score = 0
+
+startBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  screens[0].classList.add('up')
+})
+
+timeList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('time-btn')) {
+    time = +event.target.dataset.time
+    screens[1].classList.add('up')
+    startGame()
+  }
+})
+
+board.addEventListener('click', (event) => {
+  if (event.target.classList.contains('circle')) {
+    score++
+    event.target.remove()
+    createRandomCircle()
+  }
+})
+
+function startGame() {
+  setTimeout(function run() {
+    decreaseTime()
+    setTimeout(run, 1000)
+  }, 1000)
+  createRandomCircle()
+  setTime(time)
+}
+
+function decreaseTime() {
+  if (time === 0) {
+    finishGame()
+  } else {
+    let current = --time
+    if (current < 10) {
+      current = `0${current}`
+    }
+    setTime(current)
+  }
+}
+
+function setTime(value) {
+  timeEL.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+  timeEL.parentNode.classList.add('hide')
+  board.innerHTML = `<h1>Счет: <span class='primary'>${score}</span></h1>`
+}
+
+function createRandomCircle() {
+  const circle = document.createElement('div')
+  const size = getRandomNumber(10, 60)
+  const { width, height } = board.getBoundingClientRect()
+  const x = getRandomNumber(0, width - size)
+  const y = getRandomNumber(0, height - size)
+  const color = getRandomColor()
+
+  circle.classList.add('circle')
+
+  circle.style.width = `${size}px`
+  circle.style.height = `${size}px`
+  circle.style.top = `${y}px`
+  circle.style.left = `${x}px`
+  circle.style.background = color
+  circle.style.boxShadow = `0 0 2px ${color}, 0 0 15px ${color}`
+
+  board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min)
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length)
+
+  return colors[index]
+}
